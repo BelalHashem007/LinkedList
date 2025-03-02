5e278a9e3d76c898e16bf944459cab758da9abd4
BelalHashem007 <belal.hashem.saleh@gmail.com>
2025-03-02 16:26:10 +0200

    Finishing the last 3 functions in the project.
    pop, contains and find.

--- logic.js
+++ logic.js
@@ -8,6 +8,7 @@ const LinkedList = function () {
       tail = head;
     } else head = node(value, head);
   }
+
   function append(value) {
     if (head == null) prepend(value);
     else {
@@ -17,6 +18,7 @@ const LinkedList = function () {
       tail = tmp.nextNode;
     }
   }
+
   function size() {
     let tmp = head;
     let size = 0;
@@ -24,15 +26,52 @@ const LinkedList = function () {
       size++;
       tmp = tmp.nextNode;
     }
-    return console.log("Size of the linked list is : " + size);
+    return size;
   }
+
   function at(index) {
     let tmp = head;
     for (let i = 0; i < index; i++) {
       tmp = tmp.nextNode;
     }
-    return { value: tmp.value, nextNode: tmp.nextNode ? "{object}" : null };
+    return tmp;
   }
+
+  function pop() {
+    if (head == null) return;
+    if (head == tail) {
+      head = tail = null;
+    } else {
+      const nodeBeforeTail = at(size() - 2);
+      nodeBeforeTail.nextNode = null;
+      tail = nodeBeforeTail;
+    }
+  }
+
+  function contains(value) {
+    let tmp = head;
+    while (tmp != null){
+        if (tmp.value == value){
+            return true;
+        }
+        tmp = tmp.nextNode;
+    }
+    return false;
+  }
+
+  function find(value) {
+    let tmp = head;
+    let index = 0;
+    while (tmp != null){
+        if (tmp.value == value){
+            return index;
+        }
+        index++;
+        tmp = tmp.nextNode;
+    }
+    return null;
+  }
+
   function toString() {
     let tmp = head;
     while (tmp != null) {
@@ -41,13 +80,16 @@ const LinkedList = function () {
     }
     console.log("null");
   }
+
   function myHead() {
     return { value: head.value, nextNode: head.nextNode ? "{object}" : null };
   }
+
   function myTail() {
     return tail;
   }
-  return { append, prepend, size, at, toString, myHead, myTail };
+
+  return { append, prepend, size, at, toString, myHead, myTail, pop, contains, find };
 };
 
 const node = function (value = null, nextNode = null) {
--- main.js
+++ main.js
@@ -6,5 +6,7 @@ list.prepend("Ebrahim");
 list.append("Belal");
 list.append("Ahmed");
 list.toString();
-list.size();
-console.log(list.at(3))
+console.log(list.size());
+//console.log(list.contains("1"));
+console.log(list.find("Belal"));
+console.log(list.at(2))
